d6f9b25ca9e635270b691ec0f9afdf26536dc8e6
niki <nikeinikei98@gmail.com>
2022-09-18 20:51:51 +0200

    use vulkan as renderer

--- conf.lua
+++ conf.lua
@@ -2,6 +2,6 @@ function love.conf(t)
     t.window.vsync = 1
     t.window.msaa = 4
     t.window.resizable = true
-    t.renderers = {"vulkan", "opengl"}
+    t.renderers = {"vulkan"}
     t.modules.audio = false
 end
